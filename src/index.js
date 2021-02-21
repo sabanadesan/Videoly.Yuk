058c698f2d62680c3f44fb69775d5d040e14eb1c
Vinoth Sabanadesan <vinoth15@gmail.com>
2021-02-21 05:45:05 -0500

    init

---
+++ src/index.js
@@ -0,0 +1,68 @@
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const video = document.querySelector("video");
+let recorder, stream;
+
+function downloadBlob(blob, name = 'file.txt') {
+  // Convert your blob into a Blob URL (a special url that points to an object in the browser's memory)
+  const blobUrl = URL.createObjectURL(blob);
+
+  // Create a link element
+  const link = document.createElement("a");
+
+  // Set link's href to point to the Blob URL
+  link.href = blobUrl;
+  link.download = name;
+
+  // Append link to the body
+  document.body.appendChild(link);
+
+  // Dispatch click event on the link
+  // This is necessary as link.click() does not work on the latest firefox
+  link.dispatchEvent(
+    new MouseEvent('click', { 
+      bubbles: true, 
+      cancelable: true, 
+      view: window 
+    })
+  );
+
+  // Remove link from body
+  document.body.removeChild(link);
+}
+
+async function startRecording() {
+  stream = await navigator.mediaDevices.getDisplayMedia({
+    video: { mediaSource: "screen" }
+  });
+  let options = {
+    mimeType: 'video/webm;codecs=vp9,opus'
+  };
+  recorder = new MediaRecorder(stream, options);
+
+  const chunks = [];
+  recorder.ondataavailable = e => chunks.push(e.data);
+  recorder.onstop = e => {
+    const completeBlob = new Blob(chunks, { type: chunks[0].type });
+    video.src = URL.createObjectURL(completeBlob);
+
+    downloadBlob(completeBlob, 'test.webm');
+  };
+
+  recorder.start();
+}
+
+start.addEventListener("click", () => {
+  start.setAttribute("disabled", true);
+  stop.removeAttribute("disabled");
+
+  startRecording();
+});
+
+stop.addEventListener("click", () => {
+  stop.setAttribute("disabled", true);
+  start.removeAttribute("disabled");
+
+  recorder.stop();
+  stream.getVideoTracks()[0].stop();
+});
